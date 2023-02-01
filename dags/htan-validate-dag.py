792996db6116be0e590d2d54b6d81c0b5a4a4143
Brad Macdonald <bwm03570@gmail.com>
2023-02-01 15:18:26 -0700

    adds awsservice class call to htan dag

--- dags/htan-validate-dag.py
+++ dags/htan-validate-dag.py
@@ -1,13 +1,14 @@
 from datetime import datetime
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 import synapseclient
 from airflow.decorators import dag, task
 from airflow.models import Variable
 from airflow.operators.python import get_current_context
-from tempfile import TemporaryDirectory
+from services.aws_service import AWSService
 from services.nextflow_tower_service import create_and_open_tower_workspace
-from services.aws_service import upload_file_s3
-from pathlib import Path
+
 
 @dag(
     schedule_interval=None,
@@ -34,10 +35,11 @@ def htan_nf_dcqc_dag():
         temp_dir = TemporaryDirectory()
         syn_file = syn.get(syn_id, downloadLocation=temp_dir.name)
         file_path = Path(syn_file.path)
-        s3_uri = upload_file_s3(file_path=file_path, bucket_name="orca-dev-project-tower-bucket")
+        s3_uri = AWSService("tower_db_dev").upload_file_s3(
+            file_path=file_path, bucket_name="orca-dev-project-tower-bucket"
+        )
         temp_dir.cleanup()
         return s3_uri
-
 
     @task()
     def launch_tower_workflow(workspace_id: str, s3_uri: str):
@@ -48,7 +50,9 @@ def htan_nf_dcqc_dag():
             workspace_id (str): Workspace ID for tower run
             s3_uri (str): Path to S3 location of input file
         """
-        tower_utils = create_and_open_tower_workspace(platform="sage-dev", workspace_id=workspace_id)
+        tower_utils = create_and_open_tower_workspace(
+            platform="sage-dev", workspace_id=workspace_id
+        )
         tower_utils.launch_workflow(
             compute_env_id="635ROvIWp5w17QVdRy0jkk",
             pipeline="Sage-Bionetworks-Workflows/nf-dcqc",
@@ -56,10 +60,10 @@ def htan_nf_dcqc_dag():
             profiles=["docker"],
             workspace_secrets=["SYNAPSE_AUTH_TOKEN"],
             revision="main",
-            params_yaml=f'''
+            params_yaml=f"""
                 input: {s3_uri}
                 outdir: s3://orca-dev-project-tower-scratch/07_days/nf-dcqc-airflow-test/
-                '''
+                """,
         )
 
     s3_uri = stage_input_synapse_to_s3()
