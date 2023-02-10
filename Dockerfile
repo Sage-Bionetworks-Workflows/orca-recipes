104abe4a2eb9d7295a34b41b106094e9fb0fe0e9
Bruno Grande <bruno.grande@sagebase.org>
2023-02-10 05:53:22 +0000

    Draft updates for `cavatica-launch-poc`

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM apache/airflow:2.5.0-python3.10
 
+# Install Git for orca package installation (before release)
+USER root
+RUN apt update && apt install git -y
+USER airflow
+
 # #update pip
 RUN pip install --user --upgrade pip
 
--- dags/cavatica-launch-poc.py
+++ dags/cavatica-launch-poc.py
@@ -1,82 +1,56 @@
 from datetime import datetime
+from typing import Any
 
 from airflow.decorators import dag, task
-from airflow.models import Variable
+from airflow.models.param import Param
+from airflow.sensors.base import PokeReturnValue
+from orca.services.sevenbridges import SevenBridgesHook
 
-from sagetasks.sevenbridges.utils import SbgUtils
-from sagetasks.sevenbridges.general import (
-    bundle_client_args,
-    get_project_id,
-)
+params = {
+    "conn_id": Param("cavatica_test", type="string"),
+    "app_id": Param("bgrande/include-sandbox/kfdrc-rnaseq-workflow-3", type="string"),
+}
 
-
-DAG_CONFIG = {
+dag_args: dict[str, Any]
+dag_args = {
     "schedule_interval": None,
-    "start_date": datetime(2023, 1, 11),
+    "start_date": datetime(2023, 2, 10),
     "catchup": False,
     "default_args": {
         "retries": 3,
     },
     "tags": ["cavatica"],
+    "params": params,
 }
 
 
-DAG_PARAMS = {
-    "project_name": "include-sandbox",
-    "billing_group_name": "include-dev",
-    "app_id": "bgrande/include-sandbox/kfdrc-rnaseq-workflow-3",
-}
-
-
-CLIENT_ARGS = bundle_client_args(
-    auth_token=Variable.get("SB_AUTH_TOKEN"),
-    endpoint=Variable.get("SB_API_ENDPOINT")
-)
-
-
-@dag(**DAG_CONFIG)
+@dag(**dag_args)
 def cavatica_launch_poc():
-
-    @task()
-    def get_project():
-        project_name = DAG_PARAMS["project_name"]
-        billing_group_name = DAG_PARAMS["billing_group_name"]
-        project_id = get_project_id(CLIENT_ARGS, project_name, billing_group_name)
-        return project_id
-
-    @task()
-    def prepare_task(project_id: str):
-        app_id = DAG_PARAMS["app_id"]
-        utils = SbgUtils(CLIENT_ARGS)
-        utils.open_project(project_id)
-
+    @task
+    def create_task(params=None):
+        hook = SevenBridgesHook(params["conn_id"])
         task_inputs = {
             "input_type": "FASTQ",
-            'reads1': utils.client.files.get("62d99af7074179790775fda5"),
-            'reads2': utils.client.files.get("62d99afe074179790775fda9"),
+            "reads1": hook.client.files.get("62d99af7074179790775fda5"),
+            "reads2": hook.client.files.get("62d99afe074179790775fda9"),
             "runThreadN": 36,
             "wf_strand_param": "default",
             "sample_name": "HCC1187_1M",
             "rmats_read_length": 101,
             "outSAMattrRGline": "ID:HCC1187_1M\tLB:Not_Reported\tPL:Illumina\tSM:HCC1187_1M",
-            "output_basename": "cavatica_launch_poc"
+            "output_basename": "cavatica_launch_poc",
         }
+        task_id = hook.ops.create_task("{{ run_id }}", params["app_id"], task_inputs)
+        return task_id
 
-        task = utils.get_or_create_task(app_id, task_inputs, "cavatica_launch_poc")
-        return task.name
+    @task.sensor(poke_interval=60, timeout=604800, mode="reschedule")
+    def monitor_task(task_name, params=None):
+        hook = SevenBridgesHook(params["conn_id"])
+        task_status, is_done = hook.ops.get_task_status(task_name)
+        return PokeReturnValue(is_done, task_status)
 
-    @task()
-    def launch_task(project_id: str, task_name: str):
-        utils = SbgUtils(CLIENT_ARGS)
-        utils.open_project(project_id)
-        tasks = utils.get_task(task_name)
-        for task in tasks:
-            task.run()
-
-    # Explicit dependencies
-    project_id = get_project()
-    task_name = prepare_task(project_id)
-    launch_task(project_id, task_name)
+    task_id = create_task()
+    task_final_status = monitor_task(task_id)
 
 
 cavatica_launch_poc()
