d54c9c74686ce95dd86da4e2b835d5dcb7c59bed
Brad Macdonald <52762200+BWMac@users.noreply.github.com>
2023-03-13 14:49:39 -0600

    Update airflow_password.sh
    Co-authored-by: Bruno Grande <bruno.grande@sagebase.org>

--- airflow_password.sh
+++ airflow_password.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-airflow_password=$(aws --output text secretsmanager get-secret-value --secret-id airflow_password --query SecretString --region us-east-1)
-echo $airflow_password
+aws --output text secretsmanager get-secret-value --secret-id airflow_password --query SecretString --region us-east-1
