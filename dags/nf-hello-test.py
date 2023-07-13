8cf75ff58afa2cadd600d44c0f3fa642b9767550
Brad Macdonald <52762200+BWMac@users.noreply.github.com>
2023-07-13 19:53:17 +0000

    adds revision to hello

--- dags/nf-hello-test.py
+++ dags/nf-hello-test.py
@@ -32,6 +32,7 @@ def nf_hello_test_dag():
         info = LaunchInfo(
             run_name="nf-hello-test",
             pipeline="nextflow-io/hello",
+            revision="master",
         )
         run_id = hook.ops.launch_workflow(info, context["params"]["tower_compute_env_type"])
         return run_id
