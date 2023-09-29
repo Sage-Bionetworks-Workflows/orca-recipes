35ee1b2251a13b3afb0b52dfd1e70cb7a36e2cd2
Brad Macdonald <52762200+BWMac@users.noreply.github.com>
2023-09-29 15:50:12 +0000

    adds dynamic challenge dag

---
+++ dags/nf-dynamic-challenge-dag.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+
+from airflow.decorators import dag, task
+from airflow.models import Param
+
+from orca.services.nextflowtower import NextflowTowerHook
+from orca.services.nextflowtower.models import LaunchInfo
+
+
+dag_params = {
+    "tower_conn_id": Param("EXAMPLE_DEV_PROJECT_TOWER_CONN", type="string"),
+    "tower_run_name": Param("nf-dynamic-challenge-test", type="string"),
+    "tower_compute_env_type": Param("spot", type="string"),
+}
+
+dag_config = {
+    "schedule_interval": None,
+    "start_date": datetime(2023, 6, 1),
+    "catchup": False,
+    "default_args": {
+        "retries": 2,
+    },
+    "tags": ["nextflow_tower"],
+    "params": dag_params,
+}
+
+
+@dag(**dag_config)
+def nf_dynamic_challenge_dag():
+    @task()
+    def launch_nf_dynamic_challenge_on_tower(**context):
+        hook = NextflowTowerHook(context["params"]["tower_conn_id"])
+        info = LaunchInfo(
+            run_name=context["params"]["tower_run_name"],
+            pipeline="Sage-Bionetworks-Workflows/nf-dynamic-challenge",
+            revision=" bwmac/orca-273/update_workflow_inputs",
+        )
+        run_id = hook.ops.launch_workflow(
+            info, context["params"]["tower_compute_env_type"]
+        )
+        return run_id
+
+    @task.sensor(poke_interval=300, timeout=604800, mode="reschedule")
+    def monitor_nf_hello_workflow(run_id: str, **context):
+        hook = NextflowTowerHook(context["params"]["tower_conn_id"])
+        workflow = hook.ops.get_workflow(run_id)
+        print(f"Current workflow state: {workflow.status.state.value}")
+        return workflow.status.is_done
+
+    run_id = launch_nf_dynamic_challenge_on_tower()
+    monitor_nf_hello_workflow(run_id=run_id)
+
+
+nf_dynamic_challenge_dag()
