2bfbf2b4dfb6ad95f3359500ff7a9d802e472df3
Brad Macdonald <52762200+BWMac@users.noreply.github.com>
2024-01-10 19:47:49 +0000

    adds portal-data-to-snowflake-dag

--- dags/portal-data-to-snowflake-dag.py
+++ dags/portal-data-to-snowflake-dag.py
@@ -1,34 +1,47 @@
 from datetime import datetime
 
+import pandas as pd
+import synapseclient
+from orca.services.synapse import SynapseHook
+from snowflake.connector.pandas_tools import pd_writer, write_pandas
+
 from airflow.decorators import dag, task
 from airflow.models.param import Param
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+
 
 dag_params = {
     "snowflake_conn_id": Param(
         "SNOWFLAKE_DATA_ENGINEER_PORTAL_RAW_CONN", type="string"
     ),
-    # "portal_dict": Param(
-    #     {
-    #         "AD": "syn11346063",
-    #         "PSYCHENCODE": "syn20821313.16",
-    #         "NF": "syn16858331",
-    #         "ELITE": "syn51228429",
-    #         "HTAN": "syn52748752",
-    #         "GENIE": "syn52794526",
-    #     },
-    #     type="dict",
-    # ),
+    "synapse_conn_id": Param("SYNAPSE_ORCA_SERVICE_ACCOUNT_CONN", type="string"),
 }
 
 portal_dict = {
-    "AD": "syn11346063",
-    "PSYCHENCODE": "syn20821313.16",
-    "NF": "syn16858331",
-    "ELITE": "syn51228429",
-    "HTAN": "syn52748752",
-    "GENIE": "syn52794526",
+    "AD": {"synapse_id": "syn11346063", "id_col": "ID"},
+    "PSYCHENCODE": {"synapse_id": "syn20821313.16", "id_col": "ID"},
+    "NF": {"synapse_id": "syn16858331", "id_col": "ID"},
+    "ELITE": {"synapse_id": "syn51228429", "id_col": "ID"},
+    "HTAN": {"synapse_id": "syn52748752", "id_col": "ENTITYID"},
+    "GENIE": {"synapse_id": "syn52794526", "id_col": "ID"},
 }
+
+
+def prepare_merge_sql(portal_name: str, target_table: str, portal_df: pd.DataFrame, id_col: str) -> str:
+    update_set = [f'"{portal_name}"."{col}" = "{target_table}"."{col}"' for col in portal_df.columns]
+    update_set_str = ",".join(update_set)
+    col_str = ",".join(f'"{col}"' for col in portal_df.columns)
+    to_insert_str = ",".join([f'"{target_table}"."{col}"' for col in portal_df.columns])
+    merge_sql = f"""
+    MERGE INTO {portal_name} USING {target_table}
+        ON {portal_name}.{id_col} = {target_table}.{id_col}
+        when matched then
+            update set {update_set_str}
+        when not matched then
+        insert
+        ({col_str}) values({to_insert_str});
+    """
+    return merge_sql
 
 dag_config = {
     "schedule_interval": "0 0 * * 1",
@@ -45,14 +58,65 @@ dag_config = {
 @dag(**dag_config)
 def portal_data_to_snowflake():
     @task
-    def test_connection(**context):
-        snow = SnowflakeHook(context["params"]["snowflake_conn_id"])
-        # query the SAGE database in the PORTAL_RAW schema in the AD table and display the first 10 rows
-        snow.run(
-            "select * from AD limit 10",
-        )
+    def get_portal_data_from_synapse(**context):
+        syn_hook = SynapseHook(context["params"]["synapse_conn_id"])
+        for portal_name, info in portal_dict.items():
+            synapse_id = info["synapse_id"]
+            # HACK: to deal with the version number
+            if "." in synapse_id:
+                ent = syn_hook.client.get(synapse_id.split(".")[0])
+            else:
+                ent = syn_hook.client.get(synapse_id)
+            if isinstance(ent, synapseclient.EntityViewSchema):
+                portal = syn_hook.client.tableQuery(f"select * from {synapse_id}")
+                portal_df = portal.asDataFrame()
+                portal_df.reset_index(inplace=True, drop="index")
+            else:
+                # for HTAN
+                portal_df = pd.read_csv(ent.path)
+            # "grant" and "group" are reserved key words
+            portal_df.rename(
+                columns={"grant": "grants", "group": "groups"}, inplace=True
+            )
+            portal_df.columns = [col.upper() for col in portal_df.columns]
 
-    test_connection()
+            print("----------------------------------")
+            print(f" {portal_name}: {portal_df.shape}")
+            print("----------------------------------")
 
+            portal_dict[portal_name]["data"] = portal_df
+        return portal_dict
+
+    @task
+    def upsert_portal_data_to_snowflake(portal_data, **context):
+        snow_hook = SnowflakeHook(context["params"]["snowflake_conn_id"])
+        for portal_name, info in portal_data.items():
+            # Create temporary table so we can upsert
+            target_table = f"{portal_name}_TEMP"
+            print("Creating temporary table")
+            write_pandas(
+                snow_hook.get_conn(),
+                portal_data[portal_name]["data"],
+                target_table,
+                auto_create_table=True,
+                table_type="transient",
+                overwrite=True,
+                quote_identifiers=False
+            )
+            print("Creating merge sql")
+            merge_sql = prepare_merge_sql(portal_name=portal_name, portal_df=info["data"], target_table=target_table, id_col=info["id_col"])
+            # TODO account for schema changes
+            # Upsert into non-temporary tables
+            print(merge_sql)
+            print("Upserting")
+            snow_hook.run(merge_sql)
+            print("Dropping temporary table")
+            snow_hook.run(f"DROP TABLE {target_table}")
+
+
+    portal_data = get_portal_data_from_synapse()
+    upsert = upsert_portal_data_to_snowflake(portal_data=portal_data)
+
+    portal_data >> upsert
 
 portal_data_to_snowflake()
