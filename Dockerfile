05ca3cd8d323678432d1fa78dd2a4c0a9d3130fc
Brad Macdonald <52762200+BWMac@users.noreply.github.com>
2024-04-02 17:54:09 +0000

    adds new requirements and env building

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
 ARG BASE_IMAGE=apache/airflow:2.7.2-python3.10
 FROM $BASE_IMAGE
 
-RUN pip install --no-cache-dir pipenv
+RUN pip install --upgrade pip
 
-ARG PIPFOLDER=/tmp/pipfile/
+RUN pip install apache-airflow[amazon,celery,snowflake]==2.7.2 --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.7.2/constraints-3.10.txt"
 
-COPY Pipfile* "$PIPFOLDER"
-
-RUN cd "$PIPFOLDER" \
-  && pipenv install --dev --system
+COPY requirements.txt /tmp/requirements-airflow.txt
+RUN pip install --no-cache-dir -r /tmp/requirements.txt
---
+++ dev_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Usage: ./dev_setup.sh
+# Set up and activate a Python 3.10 virtual environment
+python3.10 -m venv orca-recipes-venv
+source orca-recipes-venv/bin/activate
+# Upgrade pip to latest version
+pip install --upgrade pip
+# Install airflow with constraints
+pip install apache-airflow[amazon,celery,snowflake]==2.7.2 --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.7.2/constraints-3.10.txt"
+# Install other dependencies
+pip install -r requirements-airflow.txt
+# Install dev dependencies
+pip install -r requirements-dev.txt
