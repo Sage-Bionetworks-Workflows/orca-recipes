274bcf85705be8f256802cd9ddd9dec359411f0a
Jenny Medina <medina.stsci@gmail.com>
2024-04-10 14:59:52 -0400

    Modify dag to support manifest file providing submissions

--- dags/pegs-challenge-submission-dag.py
+++ dags/pegs-challenge-submission-dag.py
@@ -1,12 +1,20 @@
+import uuid
+import os
 from datetime import datetime
 
 from airflow.decorators import dag, task
 from airflow.models import Param
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 from orca.services.nextflowtower import NextflowTowerHook
 from orca.services.nextflowtower.models import LaunchInfo
 from orca.services.synapse import SynapseHook
 
+UUID = uuid.uuid4()
+REGION_NAME = "us-east-1"
+BUCKET_NAME = "pegs-challenge-project-tower-scratch"
+FILE_NAME = f"submissions_{UUID}.csv"
+KEY = "work"
 
 dag_params = {
     "synapse_conn_id": Param("SYNAPSE_ORCA_SERVICE_ACCOUNT_CONN", type="string"),
@@ -32,24 +40,42 @@ dag_config = {
 
 @dag(**dag_config)
 def pegs_challenge_submission_dag():
-    @task.branch()
-    def check_for_new_submissions(**context):
-        """
-        Checks for new submissions
-
-        Args:
-            evaluation_id (str): Evaluation ID for challenge
-        """
+    @task
+    def get_new_submissions(**context) -> list[int]:
         hook = SynapseHook(context["params"]["synapse_conn_id"])
-        if hook.ops.monitor_evaluation_queue(
-            context["params"]["synapse_evaluation_id"]
-        ):
-            return "launch_model2data_workflow"
+        submissions = hook.ops.get_submissions_with_status(
+            context["params"]["view_id"], "RECEIVED"
+        )
+        return submissions
+
+    @task.branch()
+    def update_submission_statuses(submissions: list, **context) -> str:
+        if submissions:
+            hook = SynapseHook(context["params"]["synapse_conn_id"])
+            for submission in submissions:
+                hook.ops.update_submission_status(
+                    submission_id=submission,
+                    submission_status="EVALUATION_IN_PROGRESS",
+                )
+            return "stage_submissions_manifest"
         return "stop_dag"
 
     @task()
     def stop_dag():
         pass
+
+    @task()
+    def stage_submissions_manifest(submissions: list, **context) -> str:
+        s3_hook = S3Hook(
+            aws_conn_id=context["params"]["aws_conn_id"], region_name=REGION_NAME
+        )
+        df = pd.DataFrame({"submission_id": submissions})
+        df.to_csv(FILE_NAME, index=False)
+        s3_hook.load_file(
+            filename=FILE_NAME, key=f"{KEY}/{FILE_NAME}", bucket_name=BUCKET_NAME
+        )
+        os.remove(FILE_NAME)
+        return f"s3://{BUCKET_NAME}/{KEY}/{FILE_NAME}"
 
     @task()
     def launch_model2data_workflow(**context):
@@ -77,16 +103,15 @@ def pegs_challenge_submission_dag():
         print(f"Current workflow state: {workflow.status.state.value}")
         return workflow.status.is_done
 
-    submission_check = check_for_new_submissions()
+    submissions = get_new_submissions()
+    submissions_updated = update_submission_statuses(submissions=submissions)
     stop = stop_dag()
+    manifest_path = stage_submissions_manifest(submissions=submissions)
     run_id = launch_model2data_workflow()
     monitor = monitor_model2data_workflow(run_id=run_id)
 
-    submission_check >> [
-        run_id,
-        stop,
-    ]
-    run_id >> monitor
+    submissions >> submissions_updated >> [stop, manifest_path]
+    manifest_path >> run_id >> monitor
 
 
 pegs_challenge_submission_dag()
