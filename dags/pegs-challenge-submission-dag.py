d3ec647dfa14d2268d020845171e41c217d402d3
Jenny V Medina <jenny.medina@sagebase.org>
2024-05-08 18:10:19 +0000

    reintroduce manifest param

--- dags/pegs-challenge-submission-dag.py
+++ dags/pegs-challenge-submission-dag.py
@@ -90,6 +90,7 @@ def pegs_challenge_submission_dag():
             entry_name="MODEL_TO_DATA_CHALLENGE",
             workspace_secrets=["SYNAPSE_AUTH_TOKEN"],
             profile=context["params"]["challenge_profile"]
+            params={manifest: manifest_path}
         )
         run_id = hook.ops.launch_workflow(
             info, context["params"]["tower_compute_env_type"]
