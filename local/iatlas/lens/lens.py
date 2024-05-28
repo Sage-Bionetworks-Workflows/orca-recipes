70ac7f67d5783ef0179c278cd45454929a7ad950
bwmac <bwm03570@gmail.com>
2024-05-28 11:16:37 -0600

    updates LENS DAG with config

--- local/iatlas/lens/lens.py
+++ local/iatlas/lens/lens.py
@@ -18,18 +18,14 @@ from orca.services.nextflowtower import LaunchInfo, NextflowTowerOps
 from orca.services.synapse import SynapseOps
 
 
-LENS_TOWER_BUCKET = "s3://iatlas-project-tower-bucket/LENS"
-
-
 @dataclass
 class LENSDataset:
     """LENS dataset and relevant details.
 
     Attributes:
         id: Unique dataset identifier.
-        samplesheet: Synapse ID for LENS CSV samplesheet.
-        output_folder: Synapse ID for output folder (where workflow
-        output files will be indexed).
+        samplesheet: Synapse ID for CSV samplesheet to be used for Synstage and LENS.
+        output_folder: Synapse ID for Synindex output folder.
     """
 
     id: str
@@ -40,7 +36,9 @@ class LENSDataset:
         """Generate run name with given suffix."""
         return f"{self.id}_{suffix}"
 
-    def synstage_info(self, samplesheet_uri: str) -> LaunchInfo:
+    def synstage_info(
+        self, samplesheet_uri: str, s3_prefix: str, stage_key: str
+    ) -> LaunchInfo:
         """Generate LaunchInfo for nf-synstage."""
         run_name = self.get_run_name("synstage")
         return LaunchInfo(
@@ -51,12 +49,14 @@ class LENSDataset:
             params={
                 "input": samplesheet_uri,
                 "save_strategy": "flat",
-                "outdir": f"{LENS_TOWER_BUCKET}/PRINCE_full_fqs",
+                "outdir": f"{s3_prefix}/{stage_key}",
             },
             workspace_secrets=["SYNAPSE_AUTH_TOKEN"],
         )
 
-    def lens_info(self, samplesheet_uri: str) -> LaunchInfo:
+    def lens_info(
+        self, samplesheet_uri: str, s3_prefix: str, stage_key: str
+    ) -> LaunchInfo:
         """Generate LaunchInfo for LENS."""
         run_name = self.get_run_name("LENS")
         return LaunchInfo(
@@ -64,16 +64,16 @@ class LENSDataset:
             pipeline="https://gitlab.com/landscape-of-effective-neoantigens-software/lens_for_nf_tower",
             revision="sage",
             params={
-                "lens_dir": LENS_TOWER_BUCKET,
-                "fq_dir": f"{LENS_TOWER_BUCKET}/PRINCE_full_fqs",
-                "global_fq_dir": f"{LENS_TOWER_BUCKET}/PRINCE_full_fqs",
-                "output_dir": f"{LENS_TOWER_BUCKET}/PRINCE_full_outputs",
+                "lens_dir": s3_prefix,
+                "fq_dir": f"{s3_prefix}/{stage_key}",
+                "global_fq_dir": f"{s3_prefix}/{stage_key}",
+                "output_dir": f"{s3_prefix}/{stage_key}_outputs",
                 "manifest_path": samplesheet_uri,
             },
             workspace_secrets=["SYNAPSE_AUTH_TOKEN"],
         )
 
-    def synindex_info(self) -> LaunchInfo:
+    def synindex_info(self, s3_prefix: str, stage_key: str) -> LaunchInfo:
         """Generate LaunchInfo for nf-synindex."""
         return LaunchInfo(
             run_name=self.get_run_name("synindex"),
@@ -81,7 +81,7 @@ class LENSDataset:
             revision="main",
             entry_name="NF_SYNINDEX",
             params={
-                "s3_prefix": f"{LENS_TOWER_BUCKET}/PRINCE_full_fqs",
+                "s3_prefix": f"{s3_prefix}/{stage_key}_outputs",
                 "parent_id": self.output_folder,
             },
             workspace_secrets=["SYNAPSE_AUTH_TOKEN"],
@@ -98,14 +98,13 @@ class TowerLENSFlow(FlowSpec):
     dataset_id = Parameter(
         "dataset_id",
         type=str,
-        help="Synapse ID for a YAML file describing an RNA-seq dataset",
+        help="Synapse ID for a YAML file describing the dataset",
     )
 
     s3_prefix = Parameter(
         "s3_prefix",
         type=str,
-        help="S3 prefix for storing output files from different runs",
-        default=LENS_TOWER_BUCKET,
+        help="S3 prefix for parent S3 bucket where workflow files will be stored.",
     )
 
     def monitor_workflow(self, workflow_id):
@@ -116,14 +115,6 @@ class TowerLENSFlow(FlowSpec):
             message = f"Workflow did not complete successfully ({status})."
             raise RuntimeError(message)
         return status
-
-    def get_staged_samplesheet(self, samplesheet: str) -> str:
-        """Generate staged samplesheet based on synstage behavior."""
-        scheme, _, samplesheet_resource = samplesheet.partition("://")
-        if scheme != "s3":
-            raise ValueError("Expected an S3 URI.")
-        path = PurePosixPath(samplesheet_resource)
-        return f"{scheme}://{path.parent}/synstage/{path.name}"
 
     @step
     def start(self):
@@ -136,35 +127,40 @@ class TowerLENSFlow(FlowSpec):
         with self.synapse.fs.open(self.dataset_id, "r") as fp:
             kwargs = yaml.safe_load(fp)
         self.dataset = LENSDataset(**kwargs)
+        self.next(self.transfer_samplesheet_to_s3)
+
+    @step
+    def transfer_samplesheet_to_s3(self):
+        """Transfer raw samplesheet from Synapse to Tower S3 bucket."""
+        self.samplesheet_uri = (
+            f"{self.s3_prefix}/{self.dataset.id}/{self.dataset.id}.csv"
+        )
+        sheet_contents = self.synapse.fs.readtext(self.dataset.samplesheet)
+        self.s3.write_text(self.samplesheet_uri, sheet_contents)
+        self.next(self.launch_synstage)
+
+    @step
+    def launch_synstage(self):
+        """Launch nf-synstage to stage Synapse files in samplesheet."""
+        launch_info = self.dataset.synstage_info(
+            self.samplesheet_uri, self.s3_prefix, self.dataset_id
+        )
+        self.lens_manifest_uri = f"{self.s3_prefix}/{self.dataset.id}/{launch_info.run_name}/{self.dataset.id}.csv"
+        self.synstage_id = self.tower.launch_workflow(launch_info, "spot")
+        self.next(self.monitor_synstage)
+
+    @step
+    def monitor_synstage(self):
+        """Monitor nf-synstage workflow run (wait until done)."""
+        self.monitor_workflow(self.synstage_id)
         self.next(self.launch_lens)
-
-    # @step
-    # def transfer_samplesheet_to_s3(self):
-    #     """Transfer raw samplesheet from Synapse to Tower S3 bucket."""
-    #     self.samplesheet_uri = f"{self.s3_prefix}/{self.dataset.id}.csv"
-    #     sheet_contents = self.synapse.fs.readtext(self.dataset.samplesheet)
-    #     self.s3.write_text(self.samplesheet_uri, sheet_contents)
-    #     self.next(self.launch_synstage)
-
-    # @step
-    # def launch_synstage(self):
-    #     """Launch nf-synstage to stage Synapse files in samplesheet."""
-    #     launch_info = self.dataset.synstage_info(self.samplesheet_uri)
-    #     self.synstage_id = self.tower.launch_workflow(launch_info, "spot")
-    #     self.next(self.monitor_synstage)
-
-    # @step
-    # def monitor_synstage(self):
-    #     """Monitor nf-synstage workflow run (wait until done)."""
-    #     self.monitor_workflow(self.synstage_id)
-    #     self.next(self.launch_lens)
 
     @step
     def launch_lens(self):
         """Launch LENS workflow."""
-        # staged_uri = self.get_staged_samplesheet(self.samplesheet_uri)
-        staged_uri = "s3://iatlas-project-tower-bucket/LENS/PRINCE_full_fqs/PRINCE_full_dataset_synstage/PRINCE_full_manifest.tsv"
-        launch_info = self.dataset.lens_info(staged_uri)
+        launch_info = self.dataset.lens_info(
+            self.lens_manifest_uri, self.s3_prefix, self.dataset_id
+        )
         self.LENS_id = self.tower.launch_workflow(launch_info, "spot")
         self.next(self.monitor_lens)
 
@@ -177,7 +173,7 @@ class TowerLENSFlow(FlowSpec):
     @step
     def launch_synindex(self):
         """Launch nf-synindex to index S3 files back into Synapse."""
-        launch_info = self.dataset.synindex_info()
+        launch_info = self.dataset.synindex_info(self.s3_prefix, self.dataset_id)
         self.synindex_id = self.tower.launch_workflow(launch_info, "spot")
         self.next(self.monitor_synindex)
 
@@ -199,4 +195,6 @@ class TowerLENSFlow(FlowSpec):
 if __name__ == "__main__":
     TowerLENSFlow()
 
-# run with: python3 local/iatlas/lens/lens.py run --dataset_id syn58366876 --s3_prefix s3://iatlas-project-tower-bucket/LENS/outputs
+# run with: python3 local/iatlas/lens/lens.py run \
+# --dataset_id syn58366876 \
+# --s3_prefix s3://iatlas-project-tower-bucket/LENS \
