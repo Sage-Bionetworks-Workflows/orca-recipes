be5c5101333ee1ae564a66cbebddd35d2eac100c
Brad Macdonald <52762200+BWMac@users.noreply.github.com>
2024-08-01 17:20:52 -0600

    Update synapse-by-the-numbers-dag.py

--- dags/synapse-by-the-numbers-dag.py
+++ dags/synapse-by-the-numbers-dag.py
@@ -21,7 +21,7 @@ dag_params = {
 }
 
 dag_config = {
-    # run on the 1st of every month at midnight
+    # run on the 2nd of every month at midnight
     "schedule_interval": "0 0 2 * *",
     "start_date": datetime(2024, 7, 1),
     "catchup": False,
