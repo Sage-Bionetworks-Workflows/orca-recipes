82b1d25877575206b35bb57124c0f49a8e402dc8
Jenny V Medina <jenny.medina@sagebase.org>
2024-08-06 20:00:51 -0400

    Switch to final round for PEGS (#63)
    * Switch to final round for PEGS
    
    * Update tower_view_id
    
    Co-authored-by: Verena Chung <9377970+vpchung@users.noreply.github.com>
    
    * Point to latest nf-synapse-challenge
    
    * Email without score
    
    ---------
    
    Co-authored-by: Verena Chung <9377970+vpchung@users.noreply.github.com>

--- dags/pegs-challenge-submission-dag.py
+++ dags/pegs-challenge-submission-dag.py
@@ -19,10 +19,10 @@ KEY = "10days/pegs_challenge"
 dag_params = {
     "synapse_conn_id": Param("SYNAPSE_ORCA_SERVICE_ACCOUNT_CONN", type="string"),
     "aws_conn_id": Param("AWS_TOWER_PROD_S3_CONN", type="string"),
-    "revision": Param("0b45b1de981eb3e0c854bbbedbd6a570d95dbde2", type="string"),
-    "challenge_profile": Param("pegs_challenge_validate", type="string"),
+    "revision": Param("bd3890c0fe05dbdf2dfa89441e6e99b42e445516", type="string"),
+    "challenge_profile": Param("pegs_challenge_test", type="string"),
     "tower_conn_id": Param("PEGS_CHALLENGE_PROJECT_TOWER_CONN", type="string"),
-    "tower_view_id": Param("syn57373526", type="string"),
+    "tower_view_id": Param("syn58942525", type="string"),
     "tower_compute_env_type": Param("spot", type="string"),
     "uuid": Param(str(uuid.uuid4()), type="string")
 }
