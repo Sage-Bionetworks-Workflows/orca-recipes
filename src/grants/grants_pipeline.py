b2452e8e025bdf32cb2963c88bf6afe41b92a1ae
Jenny Medina <medina.stsci@gmail.com>
2025-12-18 17:00:16 -0500

    add functionality to load into Snowflake

--- src/grants/grants_pipeline.py
+++ src/grants/grants_pipeline.py
@@ -1,6 +1,10 @@
+import os
+import pandas as pd
 import requests
+import snowflake.connector
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from snowflake.connector.pandas_tools import write_pandas
 
 
 def step1_retrieve_grants(api_url, request_body):
@@ -34,8 +38,30 @@ def step2_preliminary_filter(grants_data):
         if not close_date <= month_from_now:
             print('this grant is eligible')
             eligible_grants.append(h)
-            
+    print(eligible_grants)
+    print("final eligible grants for keyword:", len(eligible_grants))
+    print('\n')
     return eligible_grants
+
+
+def step3_upload_to_snowflake(table_df, table_name, auto_table_create=False, overwrite=False):
+    """
+    Uploads the final table of the pipeline to Snowflake.
+    TODO: Replace this with Rixing's snowflake_utils module when that's merged
+    TODO: Update credentials to use a service user account instead of personal user account
+    """
+    conn = snowflake.connector.connect(
+                account='mqzfhld-vp00034',
+                user='jenny.medina@sagebase.org',
+                password=os.getenv("SNOWFLAKE_PAT"),
+                role='DATA_ENGINEER',
+                warehouse='COMPUTE_XSMALL',
+                database='SAGE',
+                schema='DPE'
+            )
+    
+    write_pandas(conn, table_df, table_name, auto_create_table=auto_table_create, overwrite=overwrite)
+
 
 def grants_pipeline(api_url, request_body):
     grants_data = step1_retrieve_grants(api_url, request_body)
@@ -48,6 +74,7 @@ if __name__ == "__main__":
     kwds = ['alzheimer', 'arthritis', 'autoimmune', 'cancer', 'nf', 'neurofibromatosis', 'longevity', 'elite']
     api_url = "https://api.grants.gov/v1/api/search2"
 
+    all_grants = []
     for keyword in kwds:
         print("Searching for grants with keyword:", keyword)
         body = {
@@ -59,4 +86,13 @@ if __name__ == "__main__":
                 "aln": "",
                 "fundingCategories": ""
             }
-        print(grants_pipeline(api_url, body))
+        grants = grants_pipeline(api_url, body)
+        all_grants.extend(grants)
+    
+    print("ALL GRANTS:", len(all_grants))
+    print(all_grants)
+    table_df = pd.DataFrame(all_grants)
+    print("converted to dataframe:")
+    print(table_df)
+    print("Uploading to Snowflake...")
+    #step3_upload_to_snowflake(table_df, auto_table_create=True, overwrite=True)
