3a2004801fd06fde3a8a79a380b9b6d5b164f729
Thomas Yu <thomas.yu@sagebase.org>
2026-05-09 16:29:50 -0700

    Include more functions just within src

--- dags/src/synapse_hook.py
+++ dags/src/synapse_hook.py
@@ -1,6 +1,8 @@
 import os
+from typing import Union
 
 import synapseclient
+from synapseclient.models import query, SubmissionStatus
 
 
 class SynapseHook:
@@ -17,6 +19,27 @@ class SynapseHook:
         if self._client is None:
             self._client = self._login()
         return self._client
+
+    def get_submissions_with_status(
+        self, submission_view: str, submission_status: str = "RECEIVED"
+    ) -> list[str]:
+        query(f"select * from {submission_view} limit 1", synapse_client=self.client)
+        df = query(
+            f"select id from {submission_view} where status = '{submission_status}'",
+            synapse_client=self.client,
+        )
+        return df["id"].tolist()
+
+    def update_submission_status(
+        self,
+        submission_id: Union[int, str],
+        submission_status: str,
+    ) -> None:
+        if type(submission_id) not in [str, int]:
+            raise TypeError("submission_id must be a string or int.")
+        sub_status = SubmissionStatus(id=submission_id).get()
+        sub_status.status = submission_status
+        sub_status.store()
 
     def _login(self) -> synapseclient.Synapse:
         auth_token = self._resolve_token()
