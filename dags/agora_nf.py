3fc7a950ae4a9037791fce152ae04636d6430938
Lingling <55448354+linglp@users.noreply.github.com>
2026-07-01 18:14:48 +0000

    add step to generate slack messages

--- dags/agora_nf.py
+++ dags/agora_nf.py
@@ -2,9 +2,10 @@ from datetime import datetime
 
 from airflow.decorators import dag, task
 from airflow.models.param import Param
-
+from airflow.models import Variable
 from orca.services.nextflowtower import NextflowTowerHook
 from orca.services.nextflowtower.models import LaunchInfo
+from slack_sdk import WebClient
 
 dag_params = {
     "tower_conn_id": Param("AGORA_PROJECT_TOWER_CONN", type="string"),
@@ -14,10 +15,10 @@ dag_params = {
     "revision": Param("main", type="string"),
     "profile": Param("model_ad_preprod", type="string"),
     "work_dir": Param("s3://agora-project-tower-scratch/work", type="string"),
-    "default_memory_gb": Param(32, type="integer"),
-    "large_memory_gb": Param(64, type="integer"),
-    "large_memory_datasets": Param("rna_de_individual,rna_de_aggregate", type="string"),
-    "dataset": Param("", type="string"),
+    "default_memory_gb": Param(32, type=["null", "integer"]),
+    "large_memory_gb": Param(64, type=["null", "integer"]),
+    "large_memory_datasets": Param("rna_de_individual,rna_de_aggregate", type=["null", "string"]),
+    "dataset": Param(None, type=["null", "string"]),
 }
 
 dag_config = {
@@ -43,6 +44,7 @@ def agora_nf_run_dag():
             workspace_id (str): Workspace ID for tower run
         """
         hook = NextflowTowerHook(context["params"]["tower_conn_id"])
+        dataset = context["params"].get("dataset") or ""
         info = LaunchInfo(
             run_name=context["params"]["tower_run_name"],
             pipeline=context["params"]["pipeline"],
@@ -54,7 +56,7 @@ def agora_nf_run_dag():
                 "default_memory_gb": context["params"]["default_memory_gb"],
                 "large_memory_gb": context["params"]["large_memory_gb"],
                 "large_memory_datasets": context["params"]["large_memory_datasets"],
-                "dataset": context["params"]["dataset"],
+                "dataset": dataset,
             },
         )
         run_id = hook.ops.launch_workflow(
@@ -62,15 +64,34 @@ def agora_nf_run_dag():
         )
         return run_id
 
-    # @task.sensor(poke_interval=300, timeout=604800, mode="reschedule")
-    # def monitor_nf_genie_workflow(run_id: str, **context):
-    #     hook = NextflowTowerHook(context["params"]["tower_conn_id"])
-    #     workflow = hook.ops.get_workflow(run_id)
-    #     print(f"Current workflow state: {workflow.status.state.value}")
-    #     return workflow.status.is_done
+    @task.sensor(poke_interval=300, timeout=604800, mode="reschedule")
+    def monitor_nf_genie_workflow(run_id: str, **context):
+        hook = NextflowTowerHook(context["params"]["tower_conn_id"])
+        workflow = hook.ops.get_workflow(run_id)
+        print(f"Current workflow state: {workflow.status.state.value}")
+        return workflow.status.is_done
 
+    @task
+    def generate_message(run_id: str, **context):
+        hook = NextflowTowerHook(context["params"]["tower_conn_id"])
+        workflow = hook.ops.get_workflow(run_id)
+        message = f"Tower workflow {workflow.name} ({workflow.id}) has completed with state: {workflow.status.state.value}"
+        return message
+
+    @task
+    def post_slack_messages(message: str) -> bool:
+        """Post the top downloads to the slack channel."""
+        client = WebClient(token=Variable.get("SLACK_DPE_TEAM_BOT_TOKEN"))
+        result = client.chat_postMessage(channel="test-agora-nextflow", text=message)
+        print(f"Result of posting to slack: [{result}]")
+        return result is not None
+    
     run_id = launch_agora_on_tower()
-    # monitor_nf_genie_workflow(run_id=run_id)
+    monitor_task = monitor_nf_genie_workflow(run_id=run_id)
+    message = generate_message(run_id=run_id)
+    post_to_slack = post_slack_messages(message=message)
+    
+    run_id >> monitor_task >> message >> post_to_slack
+    
 
-
-agora_nf_run_dag = agora_nf_run_dag()
+agora_nf_run_dag()
